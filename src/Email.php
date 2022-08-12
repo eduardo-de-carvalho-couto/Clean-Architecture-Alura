29ef091064ebabef04062a51a60f8ac8f4a5f2e4
Eduardo <coutoeduz@gmail.com>
2022-08-12 18:31:04 -0300

    First commit

---
+++ src/Email.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+class Email
+{
+    private string $endereco;
+
+    public function __construct(String $endereco)
+    {
+        if(filter_var($endereco, FILTER_VALIDATE_EMAIL) === false){
+            throw new \InvalidArgumentException(
+                'Endereço de e-mail inválido'
+            );
+        }
+
+        $this->endereco = $endereco;
+    }
+
+    public function __toString(): string
+    {
+        return $this->endereco;
+    }
+}
